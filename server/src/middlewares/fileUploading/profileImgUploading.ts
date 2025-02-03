e217e5801d5a35481c6e3cd4b8f0d44849c1b7cd
dharmeshlakum <lakumdharmesh60@gmail.com>
2025-02-03 11:27:25 +0530

    create middleware for profile image uploading

---
+++ server/src/middlewares/fileUploading/profileImgUploading.ts
@@ -0,0 +1,65 @@
+import { Request, Response, NextFunction } from "express";
+import multer from "multer";
+import { join } from "path";
+import errorResponseMW from "../error/errorResponse";
+
+// Image Storage
+const storage = multer.diskStorage({
+    destination: (req: Request, file: Express.Multer.File, cb: Function) => {
+        const filePath = join(__dirname, "../../assets/users");
+        cb(null, filePath);
+    },
+    filename: (req: Request, file: Express.Multer.File, cb: Function) => {
+        const fileName = file.originalname.replaceAll(" ", "-") + Date.now() + ".png";
+        cb(null, fileName);
+    }
+});
+
+// check File Type
+const fileFilter = (req: Request, file: Express.Multer.File, cb: Function) => {
+
+    if (!file.mimetype.startsWith("image/")) {
+        return cb(new Error("Only images are allowed!"), false);
+
+    };
+    cb(null, true);
+
+}
+const multerInstance = multer({
+    storage,
+    fileFilter,
+    limits: {
+        fileSize: 2 * 1024 * 1024,
+    }
+});
+
+const profileImgUploadingMW = (req: Request, res: Response, next: NextFunction) => {
+
+    try {
+        const upload = multerInstance.single("image");
+        upload(req, res, (err) => {
+
+            if (err instanceof multer.MulterError) {
+                if (err.code === 'LIMIT_FILE_SIZE') {
+                    errorResponseMW(res, 413, "File size is too large. Max allowed size is 5MB.")
+                    return;
+                }
+
+                errorResponseMW(res, 400, err.message)
+                return;
+
+            } else if (err instanceof Error) {
+                errorResponseMW(res, 400, err.message);
+
+            } else {
+                next(); // move to the next middleware
+            }
+        })
+
+    } catch (error: any) {
+        console.log("profile image file uploading middleware error :", error);
+        errorResponseMW(res, 500, "Internal server error !")
+    }
+}
+
+export default profileImgUploadingMW
