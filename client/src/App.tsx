80078fc9d498628dbfecb7b7bc98df0d4c200cf7
dharmeshlakum <lakumdharmesh60@gmail.com>
2025-02-03 14:40:18 +0530

    implement navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Navigation from "./navigation/Navigation";
 
 const App: React.FC = () => {
 
   return (
     <>
-      
+      <Navigation />
     </>
   );
 }
