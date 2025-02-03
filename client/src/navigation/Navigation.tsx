1760f527e2c279a1dee6030d3401fb7850ab5ec2
dharmeshlakum <lakumdharmesh60@gmail.com>
2025-02-03 15:20:23 +0530

    implement alert component

--- client/src/navigation/Navigation.tsx
+++ client/src/navigation/Navigation.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Alert from "../components/alert/Alert";
 
 const Navigation: React.FC = () => {
 
     return (
         <>
+            <Alert />
             <Routes>
 
             </Routes>
