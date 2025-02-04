ffe58c24c909ff961d2c0b665e60599087d5e7ee
dharmeshlakum <lakumdharmesh60@gmail.com>
2025-02-04 11:24:06 +0530

    implement static path for the images

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import cors from "cors";
 import authRouter from "./routes/auth/authRouter";
+import { join } from "path";
 
 const app = express();
 const port = process.env.PORT || 8000;
+
+// Paths
+const userImgPath = join(__dirname, "./assets/users");
+const postImgPath = join(__dirname, "./assets/images");
 
 // Middlewares
 app.use(express.json());
@@ -13,6 +18,8 @@ app.use(cors({
     methods: ["GET", "POST"],
     allowedHeaders: "*"
 }));
+app.use("/user/image", express.static(userImgPath));
+app.use("/post/image", express.static(postImgPath));
 
 // Routes
 app.use(authRouter);
