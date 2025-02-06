98d3946089df1a18e59b7d4a9c9cae3f9784365b
dharmeshlakum <lakumdharmesh60@gmail.com>
2025-02-06 14:42:54 +0530

    implement navbar

--- client/src/navigation/Navigation.tsx
+++ client/src/navigation/Navigation.tsx
@@ -1,13 +1,17 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Alert from "../components/alert/Alert";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
+import Navbar from "../components/navbar/Navabar";
 
 const Navigation: React.FC = () => {
 
+    const location = useLocation().pathname;
+
     return (
         <>
+            {(location !== "/auth/login" && location !== "/auth/signup") && <Navbar />}
             <Alert />
             <Routes>
                 <Route path="/auth/login" element={<Login />} />
